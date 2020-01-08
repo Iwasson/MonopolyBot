d4fb8115ca32f9c5b42f380ec24b4925a1820b02
John <jcbernards63@gmail.com>
2020-01-08 15:53:35 -0800

    Doubles

--- MoBot.js
+++ MoBot.js
@@ -36,6 +36,7 @@ function processCommand(receivedMessage) {
     let splitCommand = fullCommand.split(" ")           //splits the command, using spaces, into a command and then args
     let primaryCommand = splitCommand[0]                //sets the primary command
     let arguments = splitCommand.slice(1)               //sets an array of arguments
+    var result = 0;
 
     if(primaryCommand == "help" || primaryCommand == "Help") {
         helpCommand(arguments, receivedMessage)
@@ -50,7 +51,7 @@ function processCommand(receivedMessage) {
         imgCommand(arguments, receivedMessage)
     }
     else if(primaryCommand == "roll" || primaryCommand == "Roll") {
-        rollCommand(arguments, receivedMessage)
+        rollCommand(arguments, receivedMessage, result)
     }
 }
 
@@ -92,45 +93,18 @@ function initCommand(arguments, receivedMessage) {
     const attachment = new Discord.Attachment("https://i.pinimg.com/originals/70/f5/43/70f5434216f0fb0a45c4d75d83f41b5b.jpg")
     generalChannel.send(attachment)
 }
-//Tutorial @https://discordjs.guide/popular-topics/canvas.html#adding-in-text
-async function imgCommand(arguments, receivedMessage) {
-    const canvas = Canvas.createCanvas(1000, 1000);
-	const ctx = canvas.getContext('2d');
-	const background = await Canvas.loadImage('https://i.dailymail.co.uk/i/pix/2011/06/03/article-1393521-0C6047E600000578-120_964x966.jpg');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    //used to print text.. not needed right now
-	// Slightly smaller text placed above the member's display name
-	//ctx.font = '28px sans-serif';
-	//ctx.fillStyle = '#ffffff';
-    //ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
-    //================================================================================
-    /*
-    ToDo:
-        Add monopoly pieces to the board
-        Function to make them move x amount of places
-            Need to have pre mapped out spaces beacause of the big squares on the corners, cannot move a uniform amount
-            Each player needs to be the same size.
-    */
-    const avatar = await Canvas.loadImage("https://i0.wp.com/richonmoney.com/wordpress/wp-content/uploads/2016/06/monopoly-man.gif");
-    //76.9230769 => length of each square, corners are 2x that amount so to get to square 2 it would be 76.9230769*3
-    //871.1538459 is the bottom row
-	ctx.drawImage(avatar, 538.4615383, 871.1538459, 50, 50);
-
-    const attachment = new Discord.Attachment(canvas.toBuffer(), 'https://i.dailymail.co.uk/i/pix/2011/06/03/article-1393521-0C6047E600000578-120_964x966.jpg');
-    receivedMessage.reply(attachment);
-
-}
-
-function rollCommand(arguments, receivedMessage) {
+function rollCommand(arguments, receivedMessage, result) {
     let generalChannel = client.channels.get("664325321876832258")
     var die1 = getRandomInt(1, 7)
     var die2 = getRandomInt(1, 7)
 
-    var result = die1 + die2
+    result += die1 + die2
     generalChannel.send("You Rolled: " + die1 + " and " + die2 + " for a total of: " + result)
+    if (die1 == die2){
+        generalChannel.send("Doubles! Rolling again!");
+        rollCommand(arguments, receivedMessage, result)
+    }
 }
 
 function getRandomInt(min, max) {
