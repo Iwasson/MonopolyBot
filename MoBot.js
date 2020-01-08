f28e0f54e3045c663b83fa7d06c26a03dab62bcb
Iwasson <59637502+Iwasson@users.noreply.github.com>
2020-01-08 12:37:10 -0800

    Testing (#2)
    * Image manipulation
    
    * Update MoBot.js
    
    * Working version
    
    * Image
    
    Working image manipulation, prints monopoly board with a monopoly man
    
    * Update MoBot.js
    
    * Update MoBot.js
    
    Co-authored-by: jbmb36 <32378207+jbmb36@users.noreply.github.com>

--- MoBot.js
+++ MoBot.js
@@ -1,11 +1,14 @@
 const Discord = require('discord.js')
 const auth = require('./auth.json')
 const client = new Discord.Client()
+const Canvas = require('canvas')
+const {createCanvas, loadImage} = require('canvas')
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 
     client.user.setActivity("Monopoly")
+    client.user.setAvatar("https://banner2.cleanpng.com/20180614/lsj/kisspng-rich-uncle-pennybags-monopoly-party-game-monopoly-monopoly-man-5b22af6d28f393.9150592815289997891678.jpg")
 
     /*
     client.guilds.forEach((guild) => {
@@ -40,6 +43,9 @@ function processCommand(receivedMessage) {
     else if(primaryCommand == "init") {
         initCommand(arguments, receivedMessage)
     }
+    else if(primaryCommand == "img"){
+        imgCommand(arguments, receivedMessage)
+    }
 }
 
 function helpCommand(arguments, receivedMessage) {
@@ -55,6 +61,37 @@ function initCommand(arguments, receivedMessage) {
     let generalChannel = client.channels.get("664325321876832258")
     const attachment = new Discord.Attachment("https://i.pinimg.com/originals/70/f5/43/70f5434216f0fb0a45c4d75d83f41b5b.jpg")
     generalChannel.send(attachment)
+}
+//Tutorial @https://discordjs.guide/popular-topics/canvas.html#adding-in-text
+async function imgCommand(arguments, receivedMessage) {
+    const canvas = Canvas.createCanvas(1000, 1000);
+	const ctx = canvas.getContext('2d');
+	const background = await Canvas.loadImage('https://i.dailymail.co.uk/i/pix/2011/06/03/article-1393521-0C6047E600000578-120_964x966.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    //used to print text.. not needed right now
+	// Slightly smaller text placed above the member's display name
+	//ctx.font = '28px sans-serif';
+	//ctx.fillStyle = '#ffffff';
+    //ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+    //================================================================================
+    /*
+    ToDo:
+        Add monopoly pieces to the board
+        Function to make them move x amount of places
+            Need to have pre mapped out spaces beacause of the big squares on the corners, cannot move a uniform amount
+            Each player needs to be the same size.
+    */
+    const avatar = await Canvas.loadImage("https://i0.wp.com/richonmoney.com/wordpress/wp-content/uploads/2016/06/monopoly-man.gif");
+    //76.9230769 => length of each square, corners are 2x that amount so to get to square 2 it would be 76.9230769*3
+    //871.1538459 is the bottom row
+	ctx.drawImage(avatar, 538.4615383, 871.1538459, 50, 50);
+
+    const attachment = new Discord.Attachment(canvas.toBuffer(), 'https://i.dailymail.co.uk/i/pix/2011/06/03/article-1393521-0C6047E600000578-120_964x966.jpg');
+    receivedMessage.reply(attachment);
+
 }
 
 client.login(auth.token)
