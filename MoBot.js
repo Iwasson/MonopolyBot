9f1b63ab9ce6fee7b18c8485c77380abfdbfe5dc
Iwasson <Iwasson@users.noreply.github.com>
2020-01-26 11:30:56 -0800

    Update MoBot.js

--- MoBot.js
+++ MoBot.js
@@ -1,16 +1,15 @@
-const Discord = require('discord.js')//js library with commands for discord
-const auth = require('./auth.json')//auth tokens for logging into bot
-const client = new Discord.Client()//create a new discord client for the bot
+const Discord = require('discord.js')           //js library with commands for discord
+const auth = require('./auth.json')             //auth tokens for logging into bot
+const client = new Discord.Client()             //create a new discord client for the bot
 
-const Canvas = require('canvas')//used to draw on images
+const Canvas = require('canvas')                //used to draw on images
 const { createCanvas, loadImage } = require('canvas')
 const Roll = require('./roll.js')
 
-var player;//define struct
-var playerList = []//hold the players for game
-var pieces = ["car", "hat", "shoe", "thimble"]//Pieces available for use 
-var playerTurn = 0;//whos turn is it? 0 through players.length - 1
-var gameStart = false;//flag for the start of the game, allows more functions to be called once the game has started
+var player;                                     //define struct
+var playerList = []                             //hold the players for game
+var pieces = ["car", "hat", "shoe", "thimble"]  //Pieces available for use 
+var gameStart = false;                          //flag for the start of the game, allows more functions to be called once the game has started
 
 //when the bot is initialized call the other files
 client.on('ready', () => {
@@ -33,8 +32,7 @@ function processCommand(receivedMessage) {
     let primaryCommand = splitCommand[0]                //sets the primary command
     primaryCommand = primaryCommand.toLowerCase();      //sets the command to lowercase
     let arguments = splitCommand.slice(1)               //sets an array of arguments
-    var counter = 0;
-    
+
     switch (primaryCommand) {
         case 'help':
             helpCommand(arguments);
@@ -64,37 +62,10 @@ function processCommand(receivedMessage) {
             generalChannel.send("I don't understand the request")
             break
     }
-    //Check to see what command the user entered
-    /*
-    else if (primaryCommand == "roll" || primaryCommand == "Roll") {//roll function from roll.js
-        //add the value of the roll to the player pos
-        if(gameStart == true){
-            if (receivedMessage.author.id === playerList[playerTurn].playerID){
-                playerList[playerTurn].pos += Roll.rollCommand(arguments, receivedMessage, counter, generalChannel)
-                if (playerList[playerTurn].pos > 39){
-                    playerList[playerTurn].pos = (playerList[0].pos % 40);
-                    playerList[playerTurn].money += 200;
-                }
-                if (playerTurn == (playerList.length - 1)){//if it is the last player reset to first player
-                 playerTurn = 0;
-                }
-                else{
-                    ++playerTurn;//move to the next player
-                }
-            }
-            else{
-                generalChannel.send("Not your turn!")
-                }
-        }
-        else{
-            generalChannel.send("Game has not begun yet")
-        }
-    }
-    */
 }
 
 function helpCommand(arguments) {
-    if(arguments[0] != null) { arguments[0] = arguments[0].toLowerCase(); }
+    if (arguments[0] != null) { arguments[0] = arguments[0].toLowerCase(); }
     switch (arguments[0]) {
         case 'init':
             generalChannel.send("Initializes the bot for a new game of Monopoly! Include your choice of piece Ex: >intit car ");
@@ -128,7 +99,7 @@ function debug(arguments, receivedMessage) {
 
 }
 
-async function addPlayer(arguments, receivedMessage){
+async function addPlayer(arguments, receivedMessage) {
 
     if (!pieces.includes(arguments[0])) {
         generalChannel.send("Error, not a valid choice. Please pick from the list below.");
@@ -159,22 +130,15 @@ async function addPlayer(arguments, receivedMessage){
 }
 
 function initCommand(arguments, receivedMessage) {
-    if(receivedMessage == null) {return}
-    const attachment = new Discord.Attachment("assets/monopolyMan.jpg")
-    generalChannel.send(attachment)
-    var playerIn = false;
-    if (playerList.length > 0) {
-        if (playerList.find(({ playerID }) => playerID === receivedMessage.author.id)) {
-            playerIn = true;
-            generalChannel.send("Player already added")
-        }
-        else {
-            addPlayer(arguments, receivedMessage)
-        }
+    if (receivedMessage == null) { return }
+    if (playerList.find(({ playerID }) => playerID === receivedMessage.author.id)) {
+        playerIn = true;
+        generalChannel.send("Player already added")
     }
     else {
         addPlayer(arguments, receivedMessage)
     }
+
 }
 
 function saveCommand(arguments) {
@@ -187,7 +151,6 @@ function displayCommand(arguments) {
     myList.displayAll(generalChannel);
 }
 
-//Tutorial @https://discordjs.guide/popular-topics/canvas.html#adding-in-text
 async function imgCommand(arguments) {
     const canvas = Canvas.createCanvas(1000, 1000);
     const ctx = canvas.getContext('2d');
@@ -196,17 +159,12 @@ async function imgCommand(arguments) {
 
     ctx.strokeStyle = '#74037b';
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    //used to print text.. not needed right now
-    // Slightly smaller text placed above the member's display name
-    //ctx.font = '28px sans-serif';
-    //ctx.fillStyle = '#ffffff';
-    //ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
     //const avatar = await Canvas.loadImage("https://i0.wp.com/richonmoney.com/wordpress/wp-content/uploads/2016/06/monopoly-man.gif");
     //76.9230769 => length of each square, corners are 2x that amount so to get to square 2 it would be 76.9230769*3
     //871.1538459 is the bottom row
     //ctx.drawImage(avatar, 538.4615383, 871.1538459, 50, 50);
 
-    const attachment = new Discord.Attachment(canvas.toBuffer(), 'https://i.dailymail.co.uk/i/pix/2011/06/03/article-1393521-0C6047E600000578-120_964x966.jpg');
+    const attachment = new Discord.Attachment(canvas.toBuffer());
     generalChannel.send(attachment);
 
 }
